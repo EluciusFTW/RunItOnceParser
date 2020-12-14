d55e791261df10735ae844399b79f6aa7cceafc2
EluciusFTW <guybuss@westwerk-billard.de>
2020-12-14 10:35:53 +0100

    Added Dragonfruit package and rewrote entry point accordingly

--- RioParser.Console/Program.cs
+++ RioParser.Console/Program.cs
@@ -1,51 +1,37 @@
-﻿using RioParser.Console;
-using RioParser.Domain;
+﻿using RioParser.Domain;
 using RioParser.Domain.Extensions;
 using RioParser.Domain.HandHistories;
-using System.CommandLine;
-using System.CommandLine.Invocation;
+using System.Threading.Tasks;
 
-var logger = new ConsoleLogger();
-
-var reportCommand = new RootCommand("RioParser Report Generator")
+namespace RioParser.Console
 {
-    GameTypeOption(),
-    PathOption(),
-    HeroNameOption()
-};
-reportCommand.Handler = CommandHandler.Create<GameType, string, string>(GenerateReport);
+    class Program
+    {
+        private static readonly ConsoleLogger logger = new ConsoleLogger();
 
-return reportCommand.InvokeAsync(args).Result;
+        /// <param name="gameType">Gametype to Analyze. Valid values: PLO, NLH</param>
+        /// <param name="path" alias="p">Path to hand history folder</param>
+        /// <param name="hero">Name of the hero</param>
+        static async Task Main(string path = @"D:\Temp\RioHHs", string hero = "MiamiBlues", GameType gameType = GameType.PLO)
+        {
+            logger.Log("Path: " + path);
+            logger.Log("heroName: " + hero);
+            logger.Log("Assembly: " + System.Reflection.Assembly.GetExecutingAssembly().Location);
+            logger.Log("Directory: " + System.IO.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
+            
+            GenerateReport(gameType, path, hero);
+        }
 
-void GenerateReport(GameType gameType, string path, string hero)
-{
-    var handhistoryFiles = new HandHistoryFileLoader().Load(path);
-    logger.Log($"Found {handhistoryFiles.Count} files to process.");
+        static void GenerateReport(GameType gameType, string path, string hero)
+        {
+            var handhistoryFiles = new HandHistoryFileLoader().Load(path);
+            logger.Log($"Found {handhistoryFiles.Count} files to process.");
 
-    var reportsGenerator = new ReporterFactory(logger).Create(ReportType.RakeAndSplash);
-    var reports = reportsGenerator.Process(handhistoryFiles, hero, GameType.PLO);
-    logger.Log($"Finished processing {handhistoryFiles.Count} hand history files.");
+            var reportsGenerator = new ReporterFactory(logger).Create(ReportType.RakeAndSplash);
+            var reports = reportsGenerator.Process(handhistoryFiles, hero, GameType.PLO);
+            logger.Log($"Finished processing {handhistoryFiles.Count} hand history files.");
 
-    reports.ForEach(report => logger.Log(report.PrintOut()));
-};
-
-Option<GameType> GameTypeOption()
-{
-    var option = new Option<GameType>("--game", getDefaultValue: () => GameType.PLO, description: "Gametype to Analyze");
-    option.AddAlias("-g");
-    return option;
-}
-
-Option<string> PathOption()
-{
-    var option = new Option<string>("--path", getDefaultValue: () => @"C:\Users\bussg\EluciusFTW\RunItOnceParser\Sample\HandHistoryBatch", "Path to hand history folder");
-    option.AddAlias("-p");
-    return option;
-}
-
-Option<string> HeroNameOption()
-{
-    var option = new Option<string>("--hero", getDefaultValue: () => "MiamiBlues", "Name of the hero");
-    option.AddAlias("-h");
-    return option;
+            reports.ForEach(report => logger.Log(report.PrintOut()));
+        }
+    } 
 }
