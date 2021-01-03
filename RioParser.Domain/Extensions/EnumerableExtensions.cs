09a4be70c14bf88437a7ef227b8f1463bb4c7bbc
EluciusFTW <guysbuss@gmail.com>
2021-01-03 18:56:43 +0100

    Added missing extension for alternating logging

--- RioParser.Domain/Extensions/EnumerableExtensions.cs
+++ RioParser.Domain/Extensions/EnumerableExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace RioParser.Domain.Extensions
 {
@@ -12,5 +13,14 @@ namespace RioParser.Domain.Extensions
                 action(item);
             }
         }
+
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T, int> action)
+            => source
+                .Indexed()
+                .ForEach(element => action(element.Item, element.Index));
+
+        private static IEnumerable<(T Item, int Index)> Indexed<T>(this IEnumerable<T> source) 
+            => source
+                .Select((item, index) => (item, index));
     }
 }
